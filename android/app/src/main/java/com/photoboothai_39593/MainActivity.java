b385389ec6dc7ad4c7ff3efcb403d066efc4f9ba
Crowdbotics <team@crowdbotics.com>
2023-03-28 20:46:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/photoboothai_39593/MainActivity.java
@@ -0,0 +1,15 @@
+package com.photoboothai_39593;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "photoboothai_39593";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "photoboothai-39593",
+  url: "https://photoboothai-39593.botics.co",
+  api: "https://photoboothai-39593.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
